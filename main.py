663d74bd8767d763c653a9e1a31cb856bb556cc4
frankuman <oliverbolin97@gmail.com>
2023-01-27 15:10:25 +0100

    Fixed settings
    Removed a lot of global settings spaghetti variables and added a settings.json file which looks much better

--- main.py
+++ main.py
@@ -40,20 +40,32 @@ opendev = "https://review.opendev.org"
 chromium = "https://chromium-review.googlesource.com"
 
 #Globals
-global PLATFORM
-global DATE_1
-global DATE_2
-global SET_TIME_1
-global SET_TIME_2
-global UTC
-global current_settings
+
+def write_settings(settings):
+    file_name = "settings.json"
+    print(settings)
+    with open(file_name, "w") as settings_file:
+        json.dump(settings, settings_file,indent=4)
+
+def load_settings():
+    file_name = "settings.json"
+    with open(file_name, "r") as settings_file:
+        settings_str = settings_file.read()
+        settings_dict = json.loads(settings_str)
+    return settings_dict
 
 def update_current_settings():
+    settings_dict = load_settings()
+    platform = settings_dict.get("PLATFORM", "chromium")
+    date_2 = settings_dict.get("DATE_2", "")
+    date_1 = settings_dict.get("DATE_1", "")
+    time_2 = settings_dict.get("SET_TIME_2", "")
+    time_1 = settings_dict.get("SET_TIME_1", "")
     current_settings = f"""
 ---------------------------------------------------------
 Current settings
-Platform: {PLATFORM}
-Time:  {DATE_2} {SET_TIME_2} -> {DATE_1} {SET_TIME_1}
+Platform: {platform}
+Time:  {date_2} {time_2} -> {date_1} {time_1}
     """
     return(current_settings)
 def requestAPICall(url):
@@ -83,15 +95,15 @@ def generateJSON(json_response):
     with open(file_name, "w") as json_file:
         json.dump(json_response, json_file, indent=4)
 
-def generateLink(PLATFORM,date1 = current_date,date2=current_date,time1=time_1,time2=time_2,utc="0100"):
+def generateLink(PLATFORM,date1 = current_date,date2=current_date,time1=time_1,time2=time_2,UTC="0100"):
     """
     Generates a link that is used by the REST api, format is weird hopefully it doesn't break when
     more stuff is added
     """
 
      #The format is: since:"2023-01-26 21:18:00 +0100" before:"2023-01-26 21:18:30 +0100"
-    since = f"%22{date2}%20{time2}%20%2B{utc}%22" #This took a long while to figure out, but thanks to 
-    before = f"%22{date1}%20{time1}%20%2B{utc}%22"#this stackoverflow person https://stackoverflow.com/questions/53589423/gerrit-rest-api-not-working-when-query-string-contains-hour-minute-second
+    since = f"%22{date2}%20{time2}%20%2B{UTC}%22" #This took a long while to figure out, but thanks to 
+    before = f"%22{date1}%20{time1}%20%2B{UTC}%22"#this stackoverflow person https://stackoverflow.com/questions/53589423/gerrit-rest-api-not-working-when-query-string-contains-hour-minute-second
     getLINK = f"{PLATFORM}/changes/?q=since:{since}+before:{before}"
     return getLINK
 
@@ -99,24 +111,28 @@ def set_Time(): #WIP
     """
     Sets the timeframe, i.e 2022-12-22 06:00:00 -> 2022-12-22 06:15:00
     """
-    global PLATFORM
-    global DATE_1
-    global DATE_2
-    global SET_TIME_1
-    global SET_TIME_2
-    DATE_2 = str(input("Select a START date [FORMAT:2022-12-22]:"))
-    DATE_1 = str(input("Select a END date [FORMAT:2022-12-22]:"))
-    SET_TIME_2 = str(input("Select a START time in the format of [FORMAT:23:39:00]:"))
-    SET_TIME_1 = str(input("Select a END time in the format of [FORMAT:23:39:00]"))
-    utc = str(input("Select a END time in the format of [0100]:"))
-    
+    settings = load_settings()
+    date_1 = str(input("Select a START date [FORMAT:2022-12-22]:"))
+    date_2 = str(input("Select a END date [FORMAT:2022-12-22]:"))
+    time_1 = str(input("Select a START time in the format of [FORMAT:23:39:00]:"))
+    time_1 = str(input("Select a END time in the format of [FORMAT:23:39:00]"))
+    UTC2 = str(input("Select a END time in the format of [0100]:"))
+    settings["DATE_2"] = date_1
+    settings["DATE_1"] = date_2
+    settings["SET_TIME_2"] = time_1
+    settings["SET_TIME_1"] = time_2
+    settings["UTC"] = UTC2
+    write_settings(settings)
+    menu()
+
 
 platform_Options = {1: android, 2: opendev, 3: chromium}
 def set_Platform():
+    settings = load_settings()
     """
     Sets the platform the user wants to use. Only one can be selected.
     """
-    global PLATFORM
+    #global PLATFORM
     print("These platforms are available (standard is chromium)")
     print("1. " + android)
     print("2. " + opendev)
@@ -125,7 +141,8 @@ def set_Platform():
     choice = int(input("Enter your choice: "))
     if choice in platform_Options:
         print(platform_Options[choice])
-        PLATFORM = platform_Options[choice]
+        settings["PLATFORM"] = platform_Options[choice]
+        write_settings(settings)
         menu()
     if choice == 9:
         menu()
@@ -133,10 +150,17 @@ def set_Platform():
         print("Option not found in menu, try again!")
         set_Platform()
     
-def run_GPipe(DATE_1, DATE_2,SET_TIME_1,SET_TIME_2, UTC):
-
+def run_GPipe():
+    settings_dict = load_settings()
+    PLATFORM = settings_dict["PLATFORM"]
+    DATE_1 = settings_dict["DATE_1"]
+    DATE_2 = settings_dict["DATE_2"]
+    SET_TIME_1 = settings_dict["SET_TIME_1"]
+    SET_TIME_2 = settings_dict["SET_TIME_2"]
+    UTC = settings_dict["UTC"]
     getOPEN = generateLink(PLATFORM,DATE_1, DATE_2,SET_TIME_1,SET_TIME_2,UTC)
     requestAPICall(getOPEN)
+
 
     menu()
 def quit_GPipe():
@@ -157,39 +181,17 @@ def menu():
     if choice in menu_Options:
         menu_Options[choice]()
     if choice == 8:
-        run_GPipe(DATE_1, DATE_2,SET_TIME_1,SET_TIME_2,utc)
+        run_GPipe()
     else:
         print("Option not found in menu, try again!")
         menu()
 
 def main():
-    #Standards used
-    global PLATFORM
-    global DATE_1
-    global DATE_2
-    global SET_TIME_1
-    global SET_TIME_2
-    global current_settings
-    global utc
-    PLATFORM = chromium
-    DATE_1 = current_date
-    DATE_2 = current_date
-    SET_TIME_1 = time_1
-    SET_TIME_2 = time_2
-    utc = "0100"
+    #Dict with settings, and all in strings
+    settings_dict = {"PLATFORM": chromium, "DATE_1": current_date.strftime("%Y-%m-%d"), "DATE_2": current_date.strftime("%Y-%m-%d"), "SET_TIME_1": time_1, "SET_TIME_2":time_2,"UTC":"0100"}
+    write_settings(settings_dict)
     print(WELCOME)
     menu()
-
-    # time1 = "21:30:00"
-    # time2 = "21:31:00"
-    # date1 = "2023-01-26"
-    # date2 = "2023-01-26"
-    # utc = "0100"
-
-   
-
-
-    #print(f"Number of changes: {num_changes}")
 
 if __name__ == "__main__":
     main()
